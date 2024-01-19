27b2bec3e81435c091715b3f417987416383bf43
Vasilvskiy Andrey <andrey@vasilevskiy.info>
2024-01-19 16:34:53 +0300

    ver1

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from flask import render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from prometheus_flask_exporter import PrometheusMetrics
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/master/DZ11/flask-sqlite3-todo-crud/todo.db'
 db = SQLAlchemy(app)
+metrics = PrometheusMetrics(app)
+
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -31,5 +35,10 @@ def update():
     # return request.form # DEBUG REQUEST
     return redirect(url_for("index"))
 
+@app.route('/metrics')
+def main():
+    pass  # requests tracked by default
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
